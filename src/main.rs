d149370d012c0639fbd6eac2b07c56a1771a96aa
Nikos Filippakis <aesmade@gmail.com>
2019-10-05 12:11:39 +0200

    Write to VGA
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+#![feature(asm)]
 
 use core::panic::PanicInfo;
 
@@ -11,6 +12,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    let hello: &[u8] = b"Hello world!";
+    for (i, &byte) in hello.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x0b;
+        }
+    }
     loop {}
 }
 
